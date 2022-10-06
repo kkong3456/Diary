63f8761b416c0b610caaa939981d1062b4cdc761
kkong7 <kkongk@naver.com>
2022-10-06 16:25:22 +0900

    ui그리기-03 완료

---
+++ Diary/DiaryDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by admin on 2022/10/06.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+   
+
+}
