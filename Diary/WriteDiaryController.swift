11b4cd9adb50d903c531ea81854ed0ff73a138d6
kkong7 <kkongk@naver.com>
2022-10-06 15:56:01 +0900

    기본UI그리기-02

---
+++ Diary/WriteDiaryController.swift
@@ -0,0 +1,19 @@
+//
+//  WriteDiaryController.swift
+//  Diary
+//
+//  Created by admin on 2022/10/06.
+//
+
+import UIKit
+
+class WriteDiaryController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
